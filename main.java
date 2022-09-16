32f1e11cbf3b636c142b13a12349d1f37ae50963
khaled alabdullh <khaledabdullah2001104@gmail.com>
2022-09-16 19:18:41 +0200

    Create a dual set and get for Class Doctor

--- main.java
+++ main.java
@@ -22,5 +22,40 @@ class Doctor{
         if(type.length()>3)
             this.type=type;
     }
+    public void set_id(int id){
+        if(id>0){}
+            this.id=id;
+    }
+    public int get_id(){
+        return this.id;
+    }
+    public void set_name(String name){
+        if(name.length()>2)
+            this.name=name;
+    }
+    public String get_name(){
+        return this.name;
+    }
+    public void set_email(String email){
+        if(email.length()>10 && email.contains("@") && email.contains("."))
+            this.email=email;
+    }
+    public String get_email(){
+        return this.email;
+    }
+    public void set_age(int age){
+        if(age>17)
+            this.age=age;
+    }
+    public int get_age(){
+        return this.age;
+    }
+    public void set_type(String type){
+        if(type.length()>3)
+            this.type=type;
+    }
+    public String get_type(){
+        return this.type;
+    }
  
 }
