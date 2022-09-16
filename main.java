55985bf0e3ebd3a2f5beac72140ea1486d30dd44
khaled alabdullh <khaledabdullah2001104@gmail.com>
2022-09-16 19:16:33 +0200

    Create a Class Doctor and Constructor and features

--- main.java
+++ main.java
@@ -3,3 +3,24 @@ class Project{
         
     }
 }
+
+class Doctor{
+    private int id;
+    private String name;
+    private String email;
+    private int age;
+    private String type;
+    public Doctor(int id,String name,String email,int age,String type){
+        if(id>0){}
+            this.id=id;
+        if(name.length()>2)
+            this.name=name;
+        if(email.length()>10 && email.contains("@") && email.contains("."))
+            this.email=email;
+        if(age>17)
+            this.age=age;
+        if(type.length()>3)
+            this.type=type;
+    }
+ 
+}
